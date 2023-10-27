5eebcfb82e7309db0aaf2f8ee2be09c50c499025
Roukkan <kobayashidarryl@gmail.com>
2023-10-27 17:28:19 +0800

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Auth } from "./pages/auth";
 import { CreateRecipe } from "./pages/create-recipe";
